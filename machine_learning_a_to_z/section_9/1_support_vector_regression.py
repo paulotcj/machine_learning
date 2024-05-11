2f75e4c247c6bc5eea83cd71f0ecd34215358bbc
Paulo <paulotcj@gmail.com>
2024-05-11 13:50:58 -0400

    .

--- machine_learning_a_to_z/section_9/1_support_vector_regression.py
+++ machine_learning_a_to_z/section_9/1_support_vector_regression.py
@@ -24,6 +24,7 @@ print(y)
 print('----------------------------------------------')
 
 print('Feature Scaling')
+print('  for the SVR model, we need to apply feature scaling')
 
 #Normalization: x_norm = ( x - min(x) ) / ( max(x) - min(x) )
 #  good when you don't have a normal distribution or when you are unsure if you have one
@@ -35,11 +36,11 @@ print('Feature Scaling')
 #   wider range of datasets without assumptions about the distribution of the features."
 #  roughly the range can be expected to be between -3 and 3 (but realistically it can be any value)
 from sklearn.preprocessing import StandardScaler
-sc_x = StandardScaler()
-sc_y = StandardScaler()
+standard_scaler_x = StandardScaler()
+standard_scaler_y = StandardScaler()
 
-x = sc_x.fit_transform(x)
-y = sc_y.fit_transform(y)
+x = standard_scaler_x.fit_transform(x)
+y = standard_scaler_y.fit_transform(y)
 
 print('x:')
 print(x)
@@ -48,6 +49,31 @@ print('y:')
 print(y)
 print('----------------------------------------------')
 
+print('Trai the SVR model on the whole dataset')
+
+# (RBF) Radial Basis Function. The RBF kernel is a popular choice because it is able to 
+#   create non-linear decision boundaries and can model complex relationships 
+#   between the input features and the target variable.
+
+from sklearn.svm import SVR
+regressor = SVR(kernel = 'rbf')
+regressor.fit(x,y)
+
+print('----------------------------------------------')
+
+print('Predicting a new result')
+predict_from_this = 6.5
+target_to_confirm = 160_000
+print(f'    predict_from_this: {predict_from_this}')
+print(f'    target_to_confirm: {target_to_confirm}')
+
+result = standard_scaler_y.inverse_transform( #transform the fitted value back to the original scale
+        regressor.predict(  #predict the value
+            standard_scaler_x.transform([[predict_from_this]]) #transform the input value to the scale used in the model
+        ).reshape(-1,1) # -1 rows means as many rows as needed, and 1 column
+    )
+print('prediction:')
+print(result)
 
 
 
