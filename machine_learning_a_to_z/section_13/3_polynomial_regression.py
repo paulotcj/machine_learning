cf5609828fec054953ebf585852b4d7fc78cbf4a
Paulo <paulotcj@gmail.com>
2024-05-11 22:02:24 -0400

    .

---
+++ machine_learning_a_to_z/section_13/3_polynomial_regression.py
@@ -0,0 +1,121 @@
+print('Polynomial Regression')
+print(' facts: salary = $160,000 and experience = 6.5 years')
+
+# Importing the libraries
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+print('----------------------------------------------')
+print('Importing the dataset')
+
+dataset = pd.read_csv('./Position_Salaries.csv')
+x = dataset.iloc[:, 1:-1 ].values #the first column contains the positions names, and we don't need it
+y = dataset.iloc[:, -1].values #the last column contains the salaries
+
+print('x:')
+print(x)
+print('----')
+print('y:')
+print(y)
+print('----------------------------------------------')
+
+
+print('Making a Linear Regression model with the whole dataset')
+from sklearn.linear_model import LinearRegression
+linear_regression = LinearRegression()
+linear_regression.fit(x, y) # x = training data, y = target date
+
+print('----------------------------------------------')
+
+print('Making the Polynomial Regression model with the whole dataset')
+
+from sklearn.preprocessing import PolynomialFeatures
+
+#polynomial regression: y = b0 + b1*x1 + b2*x1^2 + b3*x1^3 + ... + bn*x1^n
+# so the degree is the number of x1^i terms in the equation - the higher the degree, 
+# the more complex the model
+poly_reg = PolynomialFeatures(degree = 5) #we should use 4 for a good fit
+
+
+x_poly = poly_reg.fit_transform(x)
+linear_regression_for_poly = LinearRegression()
+linear_regression_for_poly.fit(x_poly, y) # x = training data, y = target date
+
+print('x_poly:')
+print(x_poly)
+
+
+
+
+print('----------------------------------------------')
+
+print('Creating the visualisation for the Linear Regression model')
+
+plt.scatter(x,y, color = 'red')
+plt.plot(x, linear_regression.predict(x), color = 'blue')
+
+plt.title('Truth or Bluff (Linear Regression)')
+
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+
+plt.show()
+
+print('----------------------------------------------')
+
+print('Creating the visualisation for the Polynomial Regression model')
+plt.scatter(x, y, color = 'red')
+plt.plot(x, linear_regression_for_poly.predict( poly_reg.fit_transform(x) ) , color = 'blue')
+
+
+
+
+
+plt.title('Truth or Bluff (Polynomial Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+print('----------------------------------------------')
+
+
+print('Visualising the Polynomial Regression results (for higher resolution and smoother curve)')
+
+#creates an array from the minimum value of x to the maximum value of x, with steps of 0.1
+x_grid = np.arange(np.min(x), np.max(x), 0.1) 
+
+print('x_grid:')
+print(x_grid)
+
+#reshapes the array to be a 2D array with 1 column -> (rows, columns)
+x_grid = x_grid.reshape( len(x_grid), 1 )  
+# print('----')
+# print('x_grid:')
+# print(x_grid)
+
+plt.scatter(x,y, color = 'red')
+plt.plot(x_grid, linear_regression_for_poly.predict( poly_reg.fit_transform(x_grid) ) , color = 'blue')
+
+
+
+plt.title('Truth or Bluff (Polynomial Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+print('----------------------------------------------')
+print('facts: salary = $160,000 and experience = 6.5 years')
+print('----------------------------------------------')
+print('Predicting a new result with Linear Regression')
+linear_regression.predict([[6.5]])
+
+print( linear_regression.predict( [[6.5]]) )
+
+print('----------------------------------------------')
+print('Predicting a new result with Polynomial Regression')
+
+print( linear_regression_for_poly.predict( poly_reg.fit_transform( [[6.5]] ) ) )
+
+
---
+++ machine_learning_a_to_z/section_13/4_support_vector_regression.py
@@ -0,0 +1,86 @@
+print('Support Vector Regression (SVR)')
+print('----------------------------------------------')
+print('Import libraries')
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+print('----------------------------------------------')
+
+print('Import the dataset')
+dataset = pd.read_csv('./Position_Salaries.csv')
+x = dataset.iloc[:,1:-1].values
+y = dataset.iloc[:,-1].values
+
+print('x:')
+print(x)
+print('----')
+print('y:')
+print(y)
+print('----')
+y = y.reshape(len(y), 1) #reshape y to a 2D array - y.len as rows and 1 column
+print('y = y.reshape(len(y), 1):')
+print(y)
+print('----------------------------------------------')
+
+print('Feature Scaling')
+print('  for the SVR model, we need to apply feature scaling')
+
+#Normalization: x_norm = ( x - min(x) ) / ( max(x) - min(x) )
+#  good when you don't have a normal distribution or when you are unsure if you have one
+#  the range of the values will be between 0 and 1
+
+
+#Standardization: x_stand = ( x - mean(x) ) / standard_deviation(x)
+#  "often a safe choice and works well in many cases. It's less sensitive to outliers compared to normalization, and it can be applied to a 
+#   wider range of datasets without assumptions about the distribution of the features."
+#  roughly the range can be expected to be between -3 and 3 (but realistically it can be any value)
+from sklearn.preprocessing import StandardScaler
+standard_scaler_x = StandardScaler()
+standard_scaler_y = StandardScaler()
+
+x = standard_scaler_x.fit_transform(x)
+y = standard_scaler_y.fit_transform(y)
+
+print('x:')
+print(x)
+print('----')
+print('y:')
+print(y)
+print('----------------------------------------------')
+
+print('Flatten the Y array as the SVR model expects a 1D array')
+y = y.flatten() #reshape y back to a 1D array
+print(y)
+print('----------------------------------------------')
+print('Train the SVR model on the whole dataset')
+
+# (RBF) Radial Basis Function. The RBF kernel is a popular choice because it is able to 
+#   create non-linear decision boundaries and can model complex relationships 
+#   between the input features and the target variable.
+
+
+from sklearn.svm import SVR
+regressor = SVR(kernel = 'rbf')
+regressor.fit(x,y)
+
+
+print('----------------------------------------------')
+
+print('Predicting a new result')
+predict_from_this = 6.5
+target_to_confirm = 160_000
+print(f'    predict_from_this: {predict_from_this}')
+print(f'    target_to_confirm: {target_to_confirm}')
+
+result = standard_scaler_y.inverse_transform( #transform the fitted value back to the original scale
+        regressor.predict(  #predict the value
+            standard_scaler_x.transform([[predict_from_this]]) #transform the input value to the scale used in the model
+        ).reshape(-1,1) # -1 rows means as many rows as needed, and 1 column
+    )
+print('prediction:')
+print(result)
+
+
+
+
