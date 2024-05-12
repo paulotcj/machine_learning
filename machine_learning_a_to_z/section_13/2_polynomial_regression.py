9a325c8047620d194a2466720edb0948429edcc1
Paulo Costa <paulotcj@gmail.com>
2024-05-12 03:01:03 -0400

    .

--- machine_learning_a_to_z/section_13/2_polynomial_regression.py
+++ machine_learning_a_to_z/section_13/2_polynomial_regression.py
@@ -3,36 +3,36 @@ print('Polynomial Regression')
 print('----------------------------------------------')
 print('Import the libraries')
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import pandas as pd
 
 print('----------------------------------------------')
 print('Import the dataset')
-dataset = pd.read_csv('ENTER_THE_NAME_OF_YOUR_DATASET_HERE.csv')
-X = dataset.iloc[:, :-1].values
+dataset = pd.read_csv('Data.csv')
+x = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
 print('----------------------------------------------')
 print('Split the dataset into the Training set and Test set')
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
 
 print('----------------------------------------------')
 print('Train the Polynomial Regression model on the Training set')
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
-poly_reg = PolynomialFeatures(degree = 4)
-X_poly = poly_reg.fit_transform(X_train)
+poly_reg = PolynomialFeatures(degree = 5)
+x_poly = poly_reg.fit_transform(x_train)
 regressor = LinearRegression()
-regressor.fit(X_poly, y_train)
+regressor.fit(x_poly, y_train)
 
 print('----------------------------------------------')
 print('Predict the Test set results')
-y_pred = regressor.predict(poly_reg.transform(X_test))
+y_pred = regressor.predict(poly_reg.transform(x_test))
 np.set_printoptions(precision=2)
 print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
 
 print('----------------------------------------------')
 print('Evaluate the Model Performance')
 from sklearn.metrics import r2_score
-r2_score(y_test, y_pred)
+print(r2_score(y_test, y_pred))
--- machine_learning_a_to_z/section_13/3_support_vector_regression.py
+++ machine_learning_a_to_z/section_13/3_support_vector_regression.py
@@ -4,7 +4,7 @@ print('Support Vector Regression (SVR)')
 print('----------------------------------------------')
 print('Import the libraries')
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import pandas as pd
 
 print('----------------------------------------------')
@@ -17,29 +17,29 @@ y = y.reshape(len(y),1)
 print('----------------------------------------------')
 print('Split the dataset into the Training set and Test set')
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 print('----------------------------------------------')
 print('Feature Scaling')
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 sc_y = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
+x_train = sc_X.fit_transform(x_train)
 y_train = sc_y.fit_transform(y_train)
 
 print('----------------------------------------------')
 print('Train the SVR model on the Training set')
 from sklearn.svm import SVR
 regressor = SVR(kernel = 'rbf')
-regressor.fit(X_train, y_train)
+regressor.fit(x_train, y_train)
 
 print('----------------------------------------------')
 print('Predict the Test set results')
-y_pred = sc_y.inverse_transform(regressor.predict(sc_X.transform(X_test)).reshape(-1,1))
+y_pred = sc_y.inverse_transform(regressor.predict(sc_X.transform(x_test)).reshape(-1,1))
 np.set_printoptions(precision=2)
 print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
 
 print('----------------------------------------------')
 print('Evaluate the Model Performance')
 from sklearn.metrics import r2_score
-r2_score(y_test, y_pred)
+print(r2_score(y_test, y_pred))
