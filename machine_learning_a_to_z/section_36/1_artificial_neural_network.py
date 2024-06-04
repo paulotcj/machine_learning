18c007f4c05b5cbf8101d864a9bff0a26dcea46c
Paulo <Costa>
2024-06-04 15:14:33 -0400

    .

--- machine_learning_a_to_z/section_36/1_artificial_neural_network.py
+++ machine_learning_a_to_z/section_36/1_artificial_neural_network.py
@@ -1,68 +1,97 @@
-# Artificial Neural Network
+print('----------------------------------------------')
+print('Artificial Neural Network')
 
-# Importing the libraries
+print('----------------------------------------------')
+print('Importing the libraries')
+
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+exit()
+
+
 import numpy as np
 import pandas as pd
 import tensorflow as tf
 tf.__version__
 
-# Part 1 - Data Preprocessing
+print('----------------------------------------------')
+print('Part 1 - Data Preprocessing')
 
-# Importing the dataset
+print('----------------------------------------------')
+print('Importing the dataset')
 dataset = pd.read_csv('Churn_Modelling.csv')
 X = dataset.iloc[:, 3:-1].values
 y = dataset.iloc[:, -1].values
 print(X)
 print(y)
 
-# Encoding categorical data
-# Label Encoding the "Gender" column
+print('----------------------------------------------')
+print('Encoding categorical data')
+
+print('----------------------------------------------')
+print('Label Encoding the "Gender" column')
 from sklearn.preprocessing import LabelEncoder
 le = LabelEncoder()
 X[:, 2] = le.fit_transform(X[:, 2])
 print(X)
-# One Hot Encoding the "Geography" column
+
+print('----------------------------------------------')
+print('One Hot Encoding the "Geography" column')
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [1])], remainder='passthrough')
 X = np.array(ct.fit_transform(X))
 print(X)
 
-# Splitting the dataset into the Training set and Test set
+
+print('----------------------------------------------')
+print('Splitting the dataset into the Training set and Test set')
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-# Feature Scaling
+print('----------------------------------------------')
+print('Feature Scaling')
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-# Part 2 - Building the ANN
+print('----------------------------------------------')
+print('Part 2 - Building the ANN')
 
-# Initializing the ANN
+print('----------------------------------------------')
+print('Initializing the ANN')
 ann = tf.keras.models.Sequential()
 
-# Adding the input layer and the first hidden layer
+print('----------------------------------------------')
+print('Adding the input layer and the first hidden layer')
 ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
 
-# Adding the second hidden layer
+print('----------------------------------------------')
+print('Adding the second hidden layer')
 ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
 
-# Adding the output layer
+print('----------------------------------------------')
+print('Adding the output layer')
 ann.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
 
-# Part 3 - Training the ANN
+print('----------------------------------------------')
+print('Part 3 - Training the ANN')
 
-# Compiling the ANN
+print('----------------------------------------------')
+print('Compiling the ANN')
 ann.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
-# Training the ANN on the Training set
+print('----------------------------------------------')
+print('Training the ANN on the Training set')
 ann.fit(X_train, y_train, batch_size = 32, epochs = 100)
 
-# Part 4 - Making the predictions and evaluating the model
+print('----------------------------------------------')
+print('Part 4 - Making the predictions and evaluating the model')
 
-# Predicting the result of a single observation
+print('----------------------------------------------')
+print('Predicting the result of a single observation')
 
 """
 Homework:
@@ -90,12 +119,14 @@ Important note 1: Notice that the values of the features were all input in a dou
 Important note 2: Notice also that the "France" country was not input as a string in the last column but as "1, 0, 0" in the first three columns. That's because of course the predict method expects the one-hot-encoded values of the state, and as we see in the first row of the matrix of features X, "France" was encoded as "1, 0, 0". And be careful to include these values in the first three columns, because the dummy variables are always created in the first columns.
 """
 
-# Predicting the Test set results
+print('----------------------------------------------')
+print('Predicting the Test set results')
 y_pred = ann.predict(X_test)
 y_pred = (y_pred > 0.5)
 print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
 
-# Making the Confusion Matrix
+print('----------------------------------------------')
+print('Making the Confusion Matrix')
 from sklearn.metrics import confusion_matrix, accuracy_score
 cm = confusion_matrix(y_test, y_pred)
 print(cm)
