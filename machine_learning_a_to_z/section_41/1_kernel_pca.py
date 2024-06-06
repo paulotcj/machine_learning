17c31548a250ebcfafcb7359e5cb227aecdbc9a7
Paulo <paulotcj@gmail.com>
2024-06-06 14:25:39 -0400

    .

--- machine_learning_a_to_z/section_41/1_kernel_pca.py
+++ machine_learning_a_to_z/section_41/1_kernel_pca.py
@@ -1,46 +1,71 @@
-# Kernel PCA
+print('----------------------------------------------')
+print('Kernel PCA')
 
-# Importing the libraries
+print('----------------------------------------------')
+print('Importing the libraries')
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importing the dataset
+print('----------------------------------------------')
+print('Importing the dataset')
 dataset = pd.read_csv('Wine.csv')
-X = dataset.iloc[:, :-1].values
+x = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
-# Splitting the dataset into the Training set and Test set
+print('----------------------------------------------')
+print('Splitting the dataset into the Training set and Test set')
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
 
-# Feature Scaling
+print('----------------------------------------------')
+print('Feature Scaling')
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
+x_train = sc.fit_transform(x_train)
+x_test = sc.transform(x_test)
 
-# Applying Kernel PCA
+print('----------------------------------------------')
+print('Applying Kernel PCA')
 from sklearn.decomposition import KernelPCA
 kpca = KernelPCA(n_components = 2, kernel = 'rbf')
-X_train = kpca.fit_transform(X_train)
-X_test = kpca.transform(X_test)
+x_train = kpca.fit_transform(x_train)
+x_test = kpca.transform(x_test)
 
-# Training the Logistic Regression model on the Training set
+
+print('----------------------------------------------')
+print('Training the Logistic Regression model on the Training set')
 from sklearn.linear_model import LogisticRegression
 classifier = LogisticRegression(random_state = 0)
-classifier.fit(X_train, y_train)
+classifier.fit(x_train, y_train)
 
-# Making the Confusion Matrix
+
+print('----------------------------------------------')
+print('Making the Confusion Matrix')
 from sklearn.metrics import confusion_matrix, accuracy_score
-y_pred = classifier.predict(X_test)
-cm = confusion_matrix(y_test, y_pred)
-print(cm)
-accuracy_score(y_test, y_pred)
+y_pred = classifier.predict(x_test)
+# cm = confusion_matrix(y_test, y_pred)
+# print(cm)
+# accuracy_score(y_test, y_pred)
 
-# Visualising the Training set results
+
+cm_result = confusion_matrix(y_test, y_pred)
+print(cm_result)
+
+accuracy_score_result = accuracy_score(y_test, y_pred)
+
+print('----')
+print(f'    True Negatives: {cm_result[0][0]} - False Negatives: {cm_result[1][0]}')
+print(f'    True Positives: {cm_result[1][1]} - False Positives: {cm_result[0][1]}')
+print('----')
+print('Accuracy Score:')
+print(accuracy_score_result)
+
+
+print('----------------------------------------------')
+print('Visualising the Training set results')
 from matplotlib.colors import ListedColormap
-X_set, y_set = X_train, y_train
+X_set, y_set = x_train, y_train
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
                      np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
 plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
@@ -56,9 +81,11 @@ plt.ylabel('PC2')
 plt.legend()
 plt.show()
 
-# Visualising the Test set results
+
+print('----------------------------------------------')
+print('Visualising the Test set results')
 from matplotlib.colors import ListedColormap
-X_set, y_set = X_test, y_test
+X_set, y_set = x_test, y_test
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
                      np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
 plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
