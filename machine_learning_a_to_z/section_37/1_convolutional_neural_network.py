e651c791d7e7330e1d15ebaa0a47f5f90ec93a42
Paulo Costa <paulotcj@gmail.com>
2024-06-05 22:02:32 -0400

    .

--- machine_learning_a_to_z/section_37/1_convolutional_neural_network.py
+++ machine_learning_a_to_z/section_37/1_convolutional_neural_network.py
@@ -5,7 +5,7 @@ print('----------------------------------------------')
 print('Importing the libraries')
 import tensorflow as tf
 print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
-exit()
+
 # from keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 print(tf.__version__)
@@ -41,23 +41,33 @@ cnn = tf.keras.models.Sequential()
 
 print('----------------------------------------------')
 print('Step 1 - Convolution')
-cnn.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, activation='relu', input_shape=[64, 64, 3]))
+
+# note about the filters - 32 is a common choice for the number of filters in the first layer of a CNN
+cnn.add(tf.keras.layers.Conv2D(filters=32, # specifies the number of output filters in the convolution, or in other words, the number of feature detectors
+                               kernel_size=3, # height and width of the 2D convolution window - kernel size 3 means the network will use 3x3 filters
+                               activation='relu', 
+                               input_shape=[64, 64, 3]) # 64 pixels by 64 pixels, with 3 color channels
+        )
 
 print('----------------------------------------------')
 print('Step 2 - Pooling')
 cnn.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2))
 
 print('----------------------------------------------')
+
+#note about the filters - we are using the same architecture as before, therefore sticking to 32 filters
 print('Adding a second convolutional layer')
 cnn.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, activation='relu'))
 cnn.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2))
 
 print('----------------------------------------------')
 print('Step 3 - Flattening')
-cnn.add(tf.keras.layers.Flatten())
+cnn.add(tf.keras.layers.Flatten()) #this will transform the 2D array into a 1D array
 
 print('----------------------------------------------')
 print('Step 4 - Full Connection')
+
+# for units we selected 128, but this is a hyperparameter that can be tuned. Experimentally, 128 is a good starting point
 cnn.add(tf.keras.layers.Dense(units=128, activation='relu'))
 
 print('----------------------------------------------')
@@ -69,6 +79,8 @@ print('Part 3 - Training the CNN')
 
 print('----------------------------------------------')
 print('Compiling the CNN')
+
+# for loss, we have only two categories, so we use binary_crossentropy. Also note that the output layer has only 1 unit, hence binary
 cnn.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
 print('----------------------------------------------')
@@ -82,7 +94,10 @@ import numpy as np
 from keras.preprocessing import image
 test_image = image.load_img('dataset/single_prediction/cat_or_dog_1.jpg', target_size = (64, 64))
 test_image = image.img_to_array(test_image)
+
+#now we need to add an extra dimension to the image, as the predict method expects a batch
 test_image = np.expand_dims(test_image, axis = 0)
+
 result = cnn.predict(test_image)
 training_set.class_indices
 if result[0][0] == 1:
