16f2381742945c0eaf22b3589f44cd8c6aa4a1dc
Paulo Costa <paulotcj@gmail.com>
2024-06-06 23:43:02 -0400

    .

--- machine_learning_a_to_z/section_43/2_grid_search.py
+++ machine_learning_a_to_z/section_43/2_grid_search.py
@@ -89,22 +89,21 @@ parameters = [
     {'C': [0.25, 0.5, 0.75, 1], 'kernel': ['rbf'], 'gamma': [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]}
 ]
 
-
-
 grid_search = GridSearchCV(
     estimator = classifier,
     param_grid = parameters,
     scoring = 'accuracy',
-    cv = 10,
-    n_jobs = -1
+    cv = 10, # cross validation 10 folds
+    n_jobs = -1 # use all processors
 )
-
 
 grid_search.fit(x_train, y_train)
 best_accuracy = grid_search.best_score_
 best_parameters = grid_search.best_params_
 print("Best Accuracy: {:.2f} %".format(best_accuracy*100))
 print("Best Parameters:", best_parameters)
+
+exit()
 
 
 print('----------------------------------------------')
