563a1d64f8dd021478ec1d78ff7a96de9e91cfe5
Paulo Costa <paulotcj@gmail.com>
2024-06-07 02:46:19 -0400

    .

---
+++ machine_learning_a_to_z/section_44/2_catboost_file_script.py
@@ -0,0 +1,57 @@
+print('----------------------------------------------')
+print('CatBoost')
+
+print('----------------------------------------------')
+print('Run the following command in the terminal to install the apyori package: pip install catboost')
+
+print('----------------------------------------------')
+print('Importing the libraries')
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+print('----------------------------------------------')
+print('Importing the dataset')
+dataset = pd.read_csv('Data.csv')
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+print('----------------------------------------------')
+print('Splitting the dataset into the Training set and Test set')
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
+
+print('----------------------------------------------')
+print('Training CatBoost on the Training set')
+from catboost import CatBoostClassifier
+classifier = CatBoostClassifier()
+classifier.fit(x_train, y_train)
+
+
+print('----------------------------------------------')
+print('Making the Confusion Matrix')
+from sklearn.metrics import confusion_matrix, accuracy_score
+y_pred = classifier.predict(x_test)
+# cm = confusion_matrix(y_test, y_pred)
+# print(cm)
+# accuracy_score(y_test, y_pred)
+
+cm_result = confusion_matrix(y_test, y_pred)
+print(cm_result)
+
+accuracy_score_result = accuracy_score(y_test, y_pred)
+
+print('----')
+print(f'    True Negatives: {cm_result[0][0]} - False Negatives: {cm_result[1][0]}')
+print(f'    True Positives: {cm_result[1][1]} - False Positives: {cm_result[0][1]}')
+print('----')
+print('Accuracy Score:')
+print(accuracy_score_result)
+
+# exit()
+print('----------------------------------------------')
+print('Applying k-Fold Cross Validation')
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator = classifier, X = x_train, y = y_train, cv = 10)
+print("Accuracy: {:.2f} %".format(accuracies.mean()*100))
+print("Standard Deviation: {:.2f} %".format(accuracies.std()*100))
