ef671397af5ed7b72c38382bcf27712e021c7279
Paulo <paulotcj@gmail.com>
2024-07-11 21:31:08 -0400

    .

---
+++ ANN_from_scratch_in_python/2_three_neurons.py
@@ -0,0 +1,23 @@
+
+# 3 neuron with 3 inputs
+
+#inputs won't change
+inputs = [1,2,3, 2.5]
+
+#1 set of weights for each neuron
+weights1 = [0.2, 0.8, -0.5 , 1.0]
+weights2 = [0.5 , -0.91, 0.26, -0.5]
+weights3 = [-0.26, -0.27, 0.17, 0.87]
+
+#1 bias for each neuron
+bias1 = 2
+bias2 = 3
+bias3 = 0.5
+
+output = [
+    inputs[0]*weights1[0] + inputs[1]*weights1[1] + inputs[2]*weights1[2] + inputs[3]*weights1[3] + bias1 , 
+    inputs[0]*weights2[0] + inputs[1]*weights2[1] + inputs[2]*weights2[2] + inputs[3]*weights2[3] + bias2 ,
+    inputs[0]*weights3[0] + inputs[1]*weights3[1] + inputs[2]*weights3[2] + inputs[3]*weights3[3] + bias3
+]
+
+print(output)
