358573bd2e17b3a93ba975fe151821a9dd829dc9
Paulo Costa <paulotcj@gmail.com>
2024-10-04 18:47:30 -0400

    .

--- models/RNN/3_RNN.py
+++ models/RNN/3_RNN.py
@@ -82,4 +82,33 @@ print(f'  h0:{h0} - h1:{h1} -  h2:{h2} -  h3:{h3}')
 print("  Prediction from network ", y_pred_model)
 print("  Prediction from our computation ", o3)
 
-exit()
+print('----------------------------------------------')
+
+
+# Parameter split_percent defines the ratio of training examples
+def get_train_test(url, split_percent=0.8):
+    df = read_csv(url, usecols=[1], engine='python')
+    data = np.array(df.values.astype('float32'))
+
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    data = scaler.fit_transform(data).flatten()
+
+    n = len(data)
+    # Point for splitting data into train and test
+    split = int(n*split_percent)
+
+    train_data = data[range(split)]
+    test_data = data[split:]
+
+    return train_data, test_data, data
+
+sunspots_url = 'https://raw.githubusercontent.com/jbrownlee/Datasets/master/monthly-sunspots.csv'
+train_data, test_data, data = get_train_test(sunspots_url)
+
+print('-----')
+print(f'train_data first 5 rows : {train_data[0:5]}')
+print('-----')
+print(f'test_data first 5 rows : {test_data[0:5]}')
+print('-----')
+print(f'data first 5 rows : {data[0:5]}')
+print('-----')
