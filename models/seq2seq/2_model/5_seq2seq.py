47f87507e2c82fd335175872d121b8357f294970
Paulo Costa <paulotcj@gmail.com>
2024-10-08 01:37:05 -0400

    .

--- models/seq2seq/2_model/5_seq2seq.py
+++ models/seq2seq/2_model/5_seq2seq.py
@@ -10,33 +10,33 @@ import numpy as np
 
 #-------------------------------------------------------------------------
 def normalize_values(value : np.array, n_numbers : int, largest : int):
-	# In this working script we are generating random integer between 1 and 100, so the
-	#  largest unique random value is 100, and since we could also be normalizing the sum
-	#  of these numbers, the largest possible sum is 200, so imagine the examples:
+    # In this working script we are generating random integer between 1 and 100, so the
+    #  largest unique random value is 100, and since we could also be normalizing the sum
+    #  of these numbers, the largest possible sum is 200, so imagine the examples:
     #    100 / 200 = 0.5, 1 / 200 = 0.005 , 30 / 200 = 0.15, and
-	#    200 / 200 = 1
-	return value.astype('float') / float(largest * n_numbers)
+    #    200 / 200 = 1
+    return value.astype('float') / float(largest * n_numbers)
 #-------------------------------------------------------------------------
 #-------------------------------------------------------------------------
 # invert normalization
 def invert_normalization(value, n_numbers, largest):
-	return round(value * float(largest * n_numbers))
+    return round(value * float(largest * n_numbers))
 #-------------------------------------------------------------------------
 #-------------------------------------------------------------------------
 # generate examples of random integers and their sum
 def random_sum_pairs(n_examples, n_numbers, largest):
-	x, y = list(), list()
-	for i in range(n_examples):
-		in_pattern = [randint(1,largest) for _ in range(n_numbers)] #random integers between 1 and the 'largest' value
-		out_pattern = sum(in_pattern)
-		x.append(in_pattern) # these numbers are meant to be added up in y
-		y.append(out_pattern) #this is the result of the sum in x)
-	# format as NumPy arrays
-	x,y = np.array(x), np.array(y)
-	# normalize
-	x = normalize_values(x, n_numbers, largest)
-	y = normalize_values(y, n_numbers, largest)
-	return x, y
+    x, y = list(), list()
+    for i in range(n_examples):
+        in_pattern = [randint(1,largest) for _ in range(n_numbers)] #random integers between 1 and the 'largest' value
+        out_pattern = sum(in_pattern)
+        x.append(in_pattern) # these numbers are meant to be added up in y
+        y.append(out_pattern) #this is the result of the sum in x)
+    # format as NumPy arrays
+    x,y = np.array(x), np.array(y)
+    # normalize
+    x = normalize_values(x, n_numbers, largest)
+    y = normalize_values(y, n_numbers, largest)
+    return x, y
 #-------------------------------------------------------------------------
 
 n_examples, n_numbers , largest = 10_000, 2, 100
@@ -54,6 +54,12 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
 
+import os
+from keras.models import load_model
+
+
+
+
 
 # time to configure the LSTM model, the input layer will receive 1 input feature with 
 #  2 time steps (the 2 numbers to be added up)
@@ -69,18 +75,31 @@ model.compile(loss='mean_squared_error', optimizer='adam')
 
 
 print('----------------------------------------------')
-print('Training the LSTM model')
+print('Training or loadint the LSTM model')
 # train LSTM
 
 x, y = None, None
 
-for _ in range(n_epoch):
-	x, y = random_sum_pairs(n_examples=n_examples, n_numbers=n_numbers, largest=largest)
-	# print(f'x first 5 rows: {x[:5]}')
-	# print('--------')
-	# print(f'y first 5 rows: {y[:5]}')
-	x = x.reshape(n_examples, n_numbers, 1) #set the appropriate dimension, usually 100, 2, 1
-	model.fit(x, y, epochs=1, batch_size=n_batch, verbose=2)
+model_path = 'seq2seq_model.h5'
+
+if os.path.exists(model_path):
+    print('Loading the existing model...')
+    model = load_model(model_path)
+else:
+    print('Training the model...')
+    # model.fit(x_train, y_train, epochs=100, batch_size=32)
+
+    
+    for _ in range(n_epoch):
+        x, y = random_sum_pairs(n_examples=n_examples, n_numbers=n_numbers, largest=largest)
+        # print(f'x first 5 rows: {x[:5]}')
+        # print('--------')
+        # print(f'y first 5 rows: {y[:5]}')
+        x = x.reshape(n_examples, n_numbers, 1) #set the appropriate dimension, usually 100, 2, 1
+        model.fit(x, y, epochs=1, batch_size=n_batch, verbose=2)
+
+    print('Saving the model...')
+    model.save(model_path)
 
 print('----------------------------------------------')
 print('Evaluate on some new patterns')
@@ -103,14 +122,14 @@ from sklearn.metrics import mean_squared_error
 from math import sqrt
 
 expected = [ 
-		invert_normalization(value = y_i , n_numbers = n_numbers, largest = largest) 
-		for y_i in y 
-	]
+        invert_normalization(value = y_i , n_numbers = n_numbers, largest = largest) 
+        for y_i in y 
+    ]
 
 predicted = [ 
-		invert_normalization(value = result_i, n_numbers = n_numbers, largest = largest) 
-		for result_i in result[:, 0]  #all rows, first column , since this is a 2d array of n elements with 1 array of 1 element as column
-	]
+        invert_normalization(value = result_i, n_numbers = n_numbers, largest = largest) 
+        for result_i in result[:, 0]  #all rows, first column , since this is a 2d array of n elements with 1 array of 1 element as column
+    ]
 
 
 
@@ -127,10 +146,10 @@ print('    Expected  |  Predicted  |  Error   ')
 print('    ___________________________________')
 #          12345678  |  12345678   |  12345678  
 for i in range(len(expected)):
-	error = expected[i] - predicted[i]
+    error = expected[i] - predicted[i]
 
-	print(f'    {expected[i]:<8}  |  {predicted[i]:<8}   |  {error:<8}')
+    print(f'    {expected[i]:<8}  |  {predicted[i]:<8}   |  {error:<8}')
 
 
-	
+    
 
