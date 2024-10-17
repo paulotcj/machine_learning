ea66f8c0517a1315ca17966139c2a0691292cc67
Paulo Costa <paulotcj@gmail.com>
2024-10-17 13:09:42 -0400

    .

--- models/LSTM/1_model/4_lstm.py
+++ models/LSTM/1_model/4_lstm.py
@@ -172,3 +172,48 @@ print(f'The word corresponding to index 1 is \'{idx_to_word[1]}\'')
 ##  PART 4
 ##
 ##########################################################################
+
+
+def one_hot_encode(idx, vocab_size):
+    """
+    One-hot encodes a single word given its index and the size of the vocabulary.
+    
+    Args:
+     `idx`: the index of the given word
+     `vocab_size`: the size of the vocabulary
+    
+    Returns a 1-D numpy array of length `vocab_size`.
+    """
+    # Initialize the encoded array
+    one_hot = np.zeros(vocab_size)
+    
+    # Set the appropriate element to one
+    one_hot[idx] = 1.0
+
+    return one_hot
+
+
+def one_hot_encode_sequence(sequence, vocab_size):
+    """
+    One-hot encodes a sequence of words given a fixed vocabulary size.
+    
+    Args:
+     `sentence`: a list of words to encode
+     `vocab_size`: the size of the vocabulary
+     
+    Returns a 3-D numpy array of shape (num words, vocab size, 1).
+    """
+    # Encode each word in the sentence
+    encoding = np.array([one_hot_encode(word_to_idx[word], vocab_size) for word in sequence])
+
+    # Reshape encoding s.t. it has shape (num words, vocab size, 1)
+    encoding = encoding.reshape(encoding.shape[0], encoding.shape[1], 1)
+    
+    return encoding
+
+
+test_word = one_hot_encode(word_to_idx['a'], vocab_size)
+print(f'Our one-hot encoding of \'a\' has shape {test_word.shape}.')
+
+test_sentence = one_hot_encode_sequence(['a', 'b'], vocab_size)
+print(f'Our one-hot encoding of \'a b\' has shape {test_sentence.shape}.')
