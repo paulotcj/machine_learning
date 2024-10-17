a8288e6ba01091ec38bafed6785febd878c8b93b
Paulo Costa <paulotcj@gmail.com>
2024-10-17 13:07:49 -0400

    .

---
+++ models/LSTM/1_model/4_lstm.py
@@ -0,0 +1,174 @@
+import numpy as np
+
+##########################################################################
+##
+##  PART 1
+##
+##########################################################################
+# Set seed such that we always get the same dataset
+np.random.seed(42)
+#-------------------------------------------------------------------------
+def generate_dataset(num_sequences=100):
+    """
+    Generates a number of sequences as our dataset.
+    
+    Args:
+     `num_sequences`: the number of sequences to be generated.
+     
+    Returns a list of sequences.
+    """
+    samples = []
+    
+    for _ in range(num_sequences):  
+        num_tokens = np.random.randint(1, 10) # pick a number of tokens to be generated
+        # generate an equal number of 'a's and 'b's, followed by an 'EOS' token, e.g.: ['a', 'a', 'a', 'b', 'b', 'b', 'EOS']
+        sample = ['a'] * num_tokens + ['b'] * num_tokens + ['EOS']
+        samples.append(sample)
+        
+    return samples
+#-------------------------------------------------------------------------
+print('Generating dataset of sequences')
+sequences = generate_dataset()
+
+print('A single sample from the generated dataset:')
+print(sequences[0])
+print('----------------------------------------------')
+
+##########################################################################
+##
+##  PART 2
+##
+##########################################################################
+from collections import defaultdict
+#-------------------------------------------------------------------------
+def sequences_to_dicts(sequences):
+    """
+    Creates word_to_idx and idx_to_word dictionaries for a list of sequences.
+    """
+    # A bit of Python-magic to flatten a nested list 'list_param'
+    flatten = lambda list_param: [
+        item 
+        for sublist in list_param #loop over the elements of list_param creating a sublist
+        for item in sublist #loop over the elements of sublist creating an item
+    ]
+    
+    # Flatten the dataset
+    all_words = flatten(sequences)
+    
+    # Count number of word occurences
+    word_count = defaultdict(int) #defaultdict is a dictionary that returns a default value if the key is not found, here the default value is 0
+    for word in flatten(sequences):
+        word_count[word] += 1
+
+    # Sort by frequency in descending order
+    # word_count.items() returns a list of (word, count) pairs. 'l' is a tuple (word, count). Then we sort the list of tuples by the count '-l[1]'
+    # this would be better as: word_count_desc = sorted(list(word_count.items()), key=lambda tuple_item: tuple_item[1], reverse=True)
+    word_count_desc = sorted(list(word_count.items()), key=lambda l: -l[1])
+
+    # Create a list of all unique words
+    unique_words = [item[0] for item in word_count_desc]
+    
+    # Add UNK token to list of words
+    unique_words.append('UNK')
+
+    # Count number of sequences and number of unique words
+    num_sentences, vocab_size = len(sequences), len(unique_words)
+
+    # Create dictionaries so that we can go from word to index and back
+    # If a word is not in our vocabulary, we assign it to token 'UNK'
+    word_to_idx = defaultdict(lambda: num_words) #probably should be vocab_size
+    idx_to_word = defaultdict(lambda: 'UNK')
+
+    # Fill dictionaries
+    for idx, word in enumerate(unique_words):
+        # YOUR CODE HERE!
+        word_to_idx[word] = idx
+        idx_to_word[idx] = word
+
+    return word_to_idx, idx_to_word, num_sentences, vocab_size
+#-------------------------------------------------------------------------
+
+##########################################################################
+##
+##  PART 3
+##
+##########################################################################
+from torch.utils import data
+#-------------------------------------------------------------------------
+class Dataset(data.Dataset):
+    def __init__(self, inputs, targets):
+        self.inputs = inputs # x
+        self.targets = targets # y
+
+    def __len__(self):
+        # Return the size of the dataset
+        return len(self.targets)
+
+    def __getitem__(self, index):
+        # Retrieve inputs and targets at the given index
+        x = self.inputs[index]
+        y = self.targets[index]
+
+        return x, y
+#-------------------------------------------------------------------------
+#-------------------------------------------------------------------------
+def get_inputs_targets_from_sequences(sequences):
+    # Define empty lists
+    inputs, targets = [], []
+    
+    # Append inputs and targets such that both lists contain L-1 words of a sentence of length L
+    # but targets are shifted right by one so that we can predict the next word
+    # example: 'The quick brown fox jumps'
+    #    Inputs:  ["The", "quick", "brown", "fox"]
+    #    Targets: ["quick", "brown", "fox", "jumps"]
+    # The idea is to predict the next word based on the input words, so the model learns to predict 
+    # "quick" given "The", "brown" given "quick", and so on.
+    for sequence in sequences:
+        inputs.append(sequence[:-1]) # take everything except the last word -> ["The", "quick", "brown", "fox"]
+        targets.append(sequence[1:]) # take everything except the first word -> ["quick", "brown", "fox", "jumps"]
+        
+    return inputs, targets
+#-------------------------------------------------------------------------
+def create_datasets(sequences, dataset_class, p_train=0.8, p_val=0.1, p_test=0.1):
+    # Define partition sizes
+    num_train = int( len(sequences)*p_train ) #typically 80% of the data
+    num_validation = int( len(sequences)*p_val )     #typically 10% of the data
+    num_test = int( len(sequences)*p_test )   #typically 10% of the data
+
+    # Split sequences into partitions
+    sequences_train = sequences[:num_train]
+    sequences_validation = sequences[num_train:num_train+num_validation]
+    sequences_test = sequences[-num_test:]
+
+    # Get inputs and targets for each partition
+    inputs_train, targets_train = get_inputs_targets_from_sequences(sequences_train)
+    inputs_validation, targets_validation = get_inputs_targets_from_sequences(sequences_validation)
+    inputs_test, targets_test = get_inputs_targets_from_sequences(sequences_test)
+
+    # Create datasets
+    training_set = dataset_class(inputs_train, targets_train)
+    validation_set = dataset_class(inputs_validation, targets_validation)
+    test_set = dataset_class(inputs_test, targets_test)
+
+    return training_set, validation_set, test_set
+#-------------------------------------------------------------------------    
+
+training_set, validation_set, test_set = create_datasets(sequences, Dataset)
+
+print(f'We have {len(training_set)} samples in the training set.')
+print(f'We have {len(validation_set)} samples in the validation set.')
+print(f'We have {len(test_set)} samples in the test set.')
+
+
+word_to_idx, idx_to_word, num_sequences, vocab_size = sequences_to_dicts(sequences)
+
+print(f'We have {num_sequences} sentences and {len(word_to_idx)} unique tokens in our dataset (including UNK).\n')
+print('The index of \'b\' is', word_to_idx['b'])
+print(f'The word corresponding to index 1 is \'{idx_to_word[1]}\'')
+
+
+##########################################################################
+##
+##  PART 4
+##
+##########################################################################
