417e1b69777f8e819ab68cf42c76942a6c468243
Paulo Costa <paulotcj@gmail.com>
2024-11-03 14:40:48 -0500

    .

---
+++ models/rnn_char_generation/2_rnn_char_generation.py
@@ -0,0 +1,105 @@
+
+##########################################################################
+##
+##  IMPORTS
+##
+##########################################################################
+
+from io import open
+import glob
+import os
+import unicodedata
+import string
+
+##########################################################################
+##
+##  PART 1
+##
+##########################################################################
+#-------------------------------------------------------------------------
+def findFiles(path): #returns an array with the names of the files in the directory
+    return glob.glob(path) # glob is used to retrieve files and directories that match a specified pattern
+#-------------------------------------------------------------------------
+#-------------------------------------------------------------------------
+def unicodeToAscii(all_letters, input_str): # Turn a Unicode string to plain ASCII, thanks to https://stackoverflow.com/a/518232/2809427
+
+    # unicodedata.normalize('NFD', input_str) -> normalize Unicode strings. Unicode normalization is 
+    #   a process that converts text to a standard form, which can be useful for string comparison, 
+    #   searching, and other text processing tasks.
+    # In this specific call, the function normalize is being used with the normalization form 'NFD'. 
+    #   The 'NFD' stands for Normalization Form D (Canonical Decomposition). This form decomposes combined 
+    #   characters into their constituent parts. For example, a character like 'é' 
+    #   (which is a single character) would be decomposed into 'e' and an accent character.
+    #
+    # if unicodedata.category(c) != 'Mn' -> In this specific case, the code is checking if the category of 
+    #   the character c is not equal to 'Mn'. The category 'Mn' stands for "Mark, Nonspacing". Nonspacing 
+    #   marks are characters that typically combine with preceding characters and do not occupy a space 
+    #   by themselves, such as accents or diacritical marks.
+    #   By using this condition, the code is likely filtering out nonspacing marks from further processing. 
+    #   This can be useful in text processing tasks where you want to ignore or remove diacritical marks 
+    #   to simplify the text or to perform operations that are sensitive to such marks. For example, 
+    #   in text normalization or in preparing text for machine learning models, it might be necessary to 
+    #   strip out these marks to ensure consistency and accuracy.
+
+    temp = [
+        c for c in unicodedata.normalize('NFD', input_str)
+           if unicodedata.category(c) != 'Mn' and 
+           c in all_letters
+    ]
+    
+    return ''.join(temp)
+#-------------------------------------------------------------------------
+#-------------------------------------------------------------------------
+def readLines(filename, all_letters): # Read a file and split into lines
+    # .strip -> removes any leading and trailing whitespace characters
+    lines = open(filename, encoding='utf-8').read().strip().split('\n')
+
+    # for every line (name) we will convert it to ASCII, and return a list with those names
+    return [unicodeToAscii(all_letters=all_letters, input_str=line) for line in lines] 
+#-------------------------------------------------------------------------
+#-------------------------------------------------------------------------
+def execute_part1():
+
+    all_letters = string.ascii_letters + " .,;'-"
+    n_letters = len(all_letters) + 1 # Plus EOS marker
+
+    files_pattern = 'data/names/*.txt'
+    result_findFiles = findFiles(files_pattern)
+    print(f'findFiles result:\n{result_findFiles}\n' )
+    #----
+    category_lines = {}
+    all_categories = []
+
+    #-----------------------------------------
+    # Build the category_lines dictionary, a list of lines per category
+    for filename in findFiles(files_pattern):
+        category = os.path.splitext(os.path.basename(filename))[0]
+        all_categories.append(category)
+        lines = readLines(filename = filename, all_letters=all_letters)
+        category_lines[category] = lines
+    #-----------------------------------------    
+    n_categories = len(all_categories)
+
+    if n_categories == 0:
+        raise RuntimeError('Data not found. Make sure that you downloaded data '
+            'from https://download.pytorch.org/tutorial/data.zip and extract it to '
+            'the current directory.')   
+
+    print('# categories:', n_categories, all_categories)
+    print(unicodeToAscii("O'Néàl"))         
+#-------------------------------------------------------------------------
+execute_part1()
+
+
+##########################################################################
+##
+##  PART 2
+##
+##########################################################################
+
+    
+
+
+
+
+
