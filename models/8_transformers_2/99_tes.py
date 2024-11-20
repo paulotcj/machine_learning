94ad61227742ce1ee24b0bd290710c212edcd6c4
Paulo Costa <paulotcj@gmail.com>
2024-11-20 18:26:56 -0500

    .

--- models/8_transformers_2/99_tes.py
+++ models/8_transformers_2/99_tes.py
@@ -244,30 +244,31 @@ class Transformer(nn.Module):
     #-------------------------------------------------------------------------
     def __init__(self, src_vocab_size, tgt_vocab_size, d_model, num_heads, num_layers, d_ff, max_seq_length, dropout):
         super(Transformer, self).__init__()
-        self.encoder_embedding = nn.Embedding(src_vocab_size, d_model)
-        self.decoder_embedding = nn.Embedding(tgt_vocab_size, d_model)
-        self.positional_encoding = PositionalEncoding(d_model, max_seq_length)
+        self.encoder_embedding   = nn.Embedding( src_vocab_size, d_model )
+        self.decoder_embedding   = nn.Embedding( tgt_vocab_size, d_model )
+        self.positional_encoding = PositionalEncoding( d_model, max_seq_length )
 
         self.encoder_layers = nn.ModuleList([EncoderLayer(d_model, num_heads, d_ff, dropout) for _ in range(num_layers)])
         self.decoder_layers = nn.ModuleList([DecoderLayer(d_model, num_heads, d_ff, dropout) for _ in range(num_layers)])
 
-        self.fc = nn.Linear(d_model, tgt_vocab_size)
-        self.dropout = nn.Dropout(dropout)
+        self.fc      = nn.Linear( d_model, tgt_vocab_size )
+        self.dropout = nn.Dropout( dropout )
     #-------------------------------------------------------------------------
     #-------------------------------------------------------------------------
     def generate_mask(self, src, tgt):
-        src_mask = (src != 0).unsqueeze(1).unsqueeze(2)
-        tgt_mask = (tgt != 0).unsqueeze(1).unsqueeze(3)
-        seq_length = tgt.size(1)
+        src_mask    = (src != 0).unsqueeze(1).unsqueeze(2)
+        tgt_mask    = (tgt != 0).unsqueeze(1).unsqueeze(3)
+        seq_length  = tgt.size(1)
         nopeak_mask = (1 - torch.triu(torch.ones(1, seq_length, seq_length), diagonal=1)).bool()
-        tgt_mask = tgt_mask & nopeak_mask
+        tgt_mask    = tgt_mask & nopeak_mask
+
         return src_mask, tgt_mask
     #-------------------------------------------------------------------------
     #-------------------------------------------------------------------------
     def forward(self, src, tgt):
-        src_mask, tgt_mask = self.generate_mask(src, tgt)
-        src_embedded = self.dropout(self.positional_encoding(self.encoder_embedding(src)))
-        tgt_embedded = self.dropout(self.positional_encoding(self.decoder_embedding(tgt)))
+        src_mask, tgt_mask  = self.generate_mask(src, tgt)
+        src_embedded        = self.dropout(self.positional_encoding(self.encoder_embedding(src)))
+        tgt_embedded        = self.dropout(self.positional_encoding(self.decoder_embedding(tgt)))
 
         enc_output = src_embedded
         for enc_layer in self.encoder_layers:
