2099bb7cd60ce4338b192f5ed040fc7c4a228c97
Paulo Costa <paulotcj@gmail.com>
2024-11-22 19:48:34 -0500

    .

--- models/8_transformers_2/1_transformer.py
+++ models/8_transformers_2/1_transformer.py
@@ -136,24 +136,31 @@ class PositionalEncoding(nn.Module):
     #-------------------------------------------------------------------------
     def __init__(self, dim_model_input, max_seq_length):
         super(PositionalEncoding, self).__init__()
-
         # dim_model_input: The dimension of the model's input
         # max_seq_length: The maximum length of the sequence for which positional encodings are pre-computed
+
+        print(f'\nPositionalEncoding - __init__')
         
-        pe = torch.zeros(max_seq_length, dim_model_input) # tensor filled with zeros, which will be populated with positional encodings
+        pos_encodings = torch.zeros(max_seq_length, dim_model_input) # tensor filled with zeros, which will be populated with positional encodings
         
         position = torch.arange(0, max_seq_length, dtype=torch.float).unsqueeze(1) # tensor containing the position indices for each position in the sequence
         
+        print(f'pos_encodings shape: {pos_encodings.shape}')
+        print(f'position shape: {position.shape}')
+        exit()
         div_term = torch.exp(
             torch.arange(0, dim_model_input, 2).float() * -(math.log(10000.0) / dim_model_input)
         ) # term used to scale the position indices in a specific way
         
+        print(f'div_term shape: {div_term.shape}')
+        # print(f'div_term: {div_term}')
+
         # the sine function is applied to the even indices and the cosine function to the odd indices of pe
-        pe[:, 0::2] = torch.sin(position * div_term)
-        pe[:, 1::2] = torch.cos(position * div_term)
+        pos_encodings[:, 0::2] = torch.sin(position * div_term)
+        pos_encodings[:, 1::2] = torch.cos(position * div_term)
         
         # pe is registered as a buffer, which means it will be part of the module's state but will not be considered a trainable parameter
-        self.register_buffer('pe', pe.unsqueeze(0))
+        self.register_buffer('pe', pos_encodings.unsqueeze(0))
     #-------------------------------------------------------------------------
     #-------------------------------------------------------------------------
     def forward(self, x):
@@ -277,8 +284,29 @@ class Transformer(nn.Module):
         dropout: Dropout rate for regularization.        
         """
 
-        self.encoder_embedding   = nn.Embedding( src_vocab_size, dim_model_embeddings )        # Embedding layer for the source sequence.
-        self.decoder_embedding   = nn.Embedding( tgt_vocab_size, dim_model_embeddings )        # Embedding layer for the target sequence
+        print('\n\nTransformer - __init__')
+        print(f'vocab_size:            {src_vocab_size}')
+        print(f'dim_model_embeddings:  {dim_model_embeddings}')
+        print(f'num_heads:             {num_heads}')
+        print(f'num_layers:            {num_layers}')
+        print(f'dim_inner_feedforward: {dim_inner_feedforward}')
+        print(f'max_seq_length:        {max_seq_length}')
+        
+
+        self.encoder_embedding = nn.Embedding( # Embedding layer for the source sequence
+            num_embeddings  = src_vocab_size, 
+            embedding_dim   = dim_model_embeddings 
+        )    
+        print(f'\nself.encoder_embedding { self.encoder_embedding }')
+        
+
+        self.decoder_embedding = nn.Embedding( # Embedding layer for the target sequence
+            num_embeddings  = tgt_vocab_size, 
+            embedding_dim   = dim_model_embeddings 
+        )
+        print(f'self.decoder_embedding { self.decoder_embedding }')
+          
+        
         self.positional_encoding = PositionalEncoding( dim_model_embeddings, max_seq_length )  # Positional encoding component
 
         #-------
