82d133e285f463286f249eb7551e17378fb8ddb4
Paulo Costa <paulotcj@gmail.com>
2024-12-18 11:22:21 -0500

    .

--- models/8_mini_gpt/test1.py
+++ models/8_mini_gpt/test1.py
@@ -19,7 +19,7 @@ print(f'debug: {debug}')
 
 file_path = 'input.txt'
 if debug:
-    # file_path = 'C://Users//paulo//source//repos//machine_learning//models//8_mini_gpt//input.txt'
+
     file_path = 'models//8_mini_gpt//input.txt'
 
 with open(file_path, 'r', encoding='utf-8') as f:
