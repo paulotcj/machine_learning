579705aeebe2a336b724272b13bbc4008dfa866c
Paulo <paulotcj@gmail.com>
2025-01-19 22:13:53 -0500

    .

--- models/8_mini_gpt/6_gpt_2.py
+++ models/8_mini_gpt/6_gpt_2.py
@@ -316,11 +316,11 @@ class Block(nn.Module):
     def __init__(self, n_embd, n_head):
         # n_embd: embedding dimension, n_head: the number of heads we'd like
         super().__init__()
-        head_size = n_embd // n_head
-        self.sa = MultiHeadAttention(n_head, head_size)
-        self.ffwd = FeedFoward(n_embd)
-        self.ln1 = nn.LayerNorm(n_embd)
-        self.ln2 = nn.LayerNorm(n_embd)
+        head_size = n_embd // n_head # 64 // 4 = 16
+        self.sa = MultiHeadAttention(num_heads = n_head, head_size = head_size)
+        self.ffwd = FeedFoward(n_embd = n_embd)
+        self.ln1 = nn.LayerNorm(normalized_shape = n_embd)
+        self.ln2 = nn.LayerNorm(normalized_shape = n_embd)
     #-------------------------------------------------------------------------
     #-------------------------------------------------------------------------
     def forward(self, x):
