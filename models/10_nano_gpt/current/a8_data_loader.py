c4118739ea2f1458793db98c99c47ec2b6a9c3bd
Paulo <paulotcj@gmail.com>
2025-02-27 15:35:19 -0500

    .

--- models/10_nano_gpt/current/a8_data_loader.py
+++ models/10_nano_gpt/current/a8_data_loader.py
@@ -192,8 +192,9 @@ def get_device():
 device = get_device()
 
 import tiktoken
-
+#-------------------------------------------------------------------------
 class DataLoaderLite:
+    #-------------------------------------------------------------------------
     def __init__(self, B, T):
         self.B = B
         self.T = T
@@ -209,7 +210,8 @@ class DataLoaderLite:
 
         # state
         self.current_position = 0
-
+    #-------------------------------------------------------------------------
+    #-------------------------------------------------------------------------
     def next_batch(self):
         B, T = self.B, self.T
         buf = self.tokens[self.current_position : self.current_position+B*T+1]
@@ -221,8 +223,8 @@ class DataLoaderLite:
         if self.current_position + (B * T + 1) > len(self.tokens):
             self.current_position = 0
         return x, y
-
-# -----------------------------------------------------------------------------
+    #-------------------------------------------------------------------------
+#-------------------------------------------------------------------------
 
 train_loader = DataLoaderLite(B=4, T=32)
 
