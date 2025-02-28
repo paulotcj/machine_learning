6291683d2a3c107d340d8026aad43a1869505ac7
paulotcj <paulotcj@gmail.com>
2025-02-27 21:47:01 -0500

    .

--- models/10_nano_gpt/current/fineweb.py
+++ models/10_nano_gpt/current/fineweb.py
@@ -14,7 +14,7 @@ import tiktoken
 from datasets import load_dataset # pip install datasets
 from tqdm import tqdm # pip install tqdm
 
-# ------------------------------------------
+#-------------------------------------------------------------------------
 local_dir = "edu_fineweb10B"
 remote_name = "sample-10BT"
 shard_size = int(1e8) # 100M tokens per shard, total of 100 shards
@@ -29,6 +29,7 @@ fw = load_dataset("HuggingFaceFW/fineweb-edu", name=remote_name, split="train")
 # init the tokenizer
 enc = tiktoken.get_encoding("gpt2")
 eot = enc._special_tokens['<|endoftext|>'] # end of text token
+#-------------------------------------------------------------------------
 def tokenize(doc):
     # tokenizes a single document and returns a numpy array of uint16 tokens
     tokens = [eot] # the special <|endoftext|> token delimits all documents
@@ -37,18 +38,26 @@ def tokenize(doc):
     assert (0 <= tokens_np).all() and (tokens_np < 2**16).all(), "token dictionary too large for uint16"
     tokens_np_uint16 = tokens_np.astype(np.uint16)
     return tokens_np_uint16
-
+#-------------------------------------------------------------------------
+#-------------------------------------------------------------------------
 def write_datafile(filename, tokens_np):
     np.save(filename, tokens_np)
+#-------------------------------------------------------------------------
+
+
 
 # tokenize all documents and write output shards, each of shard_size tokens (last shard has remainder)
 nprocs = max(1, os.cpu_count()//2)
+
+#-------------------------------------------------------------------------
 with mp.Pool(nprocs) as pool:
     shard_index = 0
     # preallocate buffer to hold current shard
     all_tokens_np = np.empty((shard_size,), dtype=np.uint16)
     token_count = 0
     progress_bar = None
+
+    #---------------------------------------------------
     for tokens in pool.imap(tokenize, fw, chunksize=16):
 
         # is there enough space in the current shard for the new tokens?
@@ -74,9 +83,13 @@ with mp.Pool(nprocs) as pool:
             # populate the next shard with the leftovers of the current doc
             all_tokens_np[0:len(tokens)-remainder] = tokens[remainder:]
             token_count = len(tokens)-remainder
+    #---------------------------------------------------
 
     # write any remaining tokens as the last shard
     if token_count != 0:
         split = "val" if shard_index == 0 else "train"
         filename = os.path.join(DATA_CACHE_DIR, f"edufineweb_{split}_{shard_index:06d}")
         write_datafile(filename, all_tokens_np[:token_count])
+
+
+#-------------------------------------------------------------------------
