e3468291a67297d67128424b13cd7f0af7d4a1ac
paulotcj <paulotcj@gmail.com>
2025-02-28 19:09:18 -0500

    .

--- models/10_nano_gpt/current/a22_validation_data_split_validation_loss_sampling_revive.py
+++ models/10_nano_gpt/current/a22_validation_data_split_validation_loss_sampling_revive.py
@@ -7,10 +7,9 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
-# -----------------------------------------------------------------------------
-
+#-------------------------------------------------------------------------
 class CausalSelfAttention(nn.Module):
-
+    #-------------------------------------------------------------------------
     def __init__(self, config):
         super().__init__()
         assert config.n_embd % config.n_head == 0
@@ -25,7 +24,8 @@ class CausalSelfAttention(nn.Module):
         # not really a 'bias', more of a mask, but following the OpenAI/HF naming though
         self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
                                      .view(1, 1, config.block_size, config.block_size))
-
+    #-------------------------------------------------------------------------
+    #-------------------------------------------------------------------------
     def forward(self, x):
         B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
         # calculate query, key, values for all heads in batch and move head forward to be the batch dim
@@ -41,36 +41,44 @@ class CausalSelfAttention(nn.Module):
         # output projection
         y = self.c_proj(y)
         return y
-
+    #-------------------------------------------------------------------------
+#-------------------------------------------------------------------------
+#-------------------------------------------------------------------------
 class MLP(nn.Module):
-
+    #-------------------------------------------------------------------------
     def __init__(self, config):
         super().__init__()
         self.c_fc    = nn.Linear(config.n_embd, 4 * config.n_embd)
         self.gelu    = nn.GELU(approximate='tanh')
         self.c_proj  = nn.Linear(4 * config.n_embd, config.n_embd)
         self.c_proj.NANOGPT_SCALE_INIT = 1
-
+    #-------------------------------------------------------------------------
+    #-------------------------------------------------------------------------
     def forward(self, x):
         x = self.c_fc(x)
         x = self.gelu(x)
         x = self.c_proj(x)
         return x
-
+    #-------------------------------------------------------------------------
+#-------------------------------------------------------------------------
+#-------------------------------------------------------------------------
 class Block(nn.Module):
-
+    #-------------------------------------------------------------------------
     def __init__(self, config):
         super().__init__()
         self.ln_1 = nn.LayerNorm(config.n_embd)
         self.attn = CausalSelfAttention(config)
         self.ln_2 = nn.LayerNorm(config.n_embd)
         self.mlp = MLP(config)
-
+    #-------------------------------------------------------------------------
+    #-------------------------------------------------------------------------
     def forward(self, x):
         x = x + self.attn(self.ln_1(x))
         x = x + self.mlp(self.ln_2(x))
         return x
-
+    #-------------------------------------------------------------------------
+#-------------------------------------------------------------------------
+#-------------------------------------------------------------------------
 @dataclass
 class GPTConfig:
     block_size: int = 1024 # max sequence length
@@ -78,9 +86,10 @@ class GPTConfig:
     n_layer: int = 12 # number of layers
     n_head: int = 12 # number of heads
     n_embd: int = 768 # embedding dimension
-
+#-------------------------------------------------------------------------
+#-------------------------------------------------------------------------
 class GPT(nn.Module):
-
+    #-------------------------------------------------------------------------
     def __init__(self, config):
         super().__init__()
         self.config = config
@@ -98,7 +107,8 @@ class GPT(nn.Module):
 
         # init params
         self.apply(self._init_weights)
-
+    #-------------------------------------------------------------------------
+    #-------------------------------------------------------------------------
     def _init_weights(self, module):
         if isinstance(module, nn.Linear):
             std = 0.02
@@ -109,7 +119,8 @@ class GPT(nn.Module):
                 torch.nn.init.zeros_(module.bias)
         elif isinstance(module, nn.Embedding):
             torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
-
+    #-------------------------------------------------------------------------
+    #-------------------------------------------------------------------------
     def forward(self, idx, targets=None):
         # idx is of shape (B, T)
         B, T = idx.size()
@@ -129,7 +140,8 @@ class GPT(nn.Module):
         if targets is not None:
             loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1))
         return logits, loss
-
+    #-------------------------------------------------------------------------
+    #-------------------------------------------------------------------------
     @classmethod
     def from_pretrained(cls, model_type):
         """Loads pretrained GPT-2 model weights from huggingface"""
@@ -178,7 +190,8 @@ class GPT(nn.Module):
                     sd[k].copy_(sd_hf[k])
 
         return model
-
+    #-------------------------------------------------------------------------
+    #-------------------------------------------------------------------------
     def configure_optimizers(self, weight_decay, learning_rate, device):
         # start with all of the candidate parameters (that require grad)
         param_dict = {pn: p for pn, p in self.named_parameters()}
@@ -203,17 +216,21 @@ class GPT(nn.Module):
             print(f"using fused AdamW: {use_fused}")
         optimizer = torch.optim.AdamW(optim_groups, lr=learning_rate, betas=(0.9, 0.95), eps=1e-8, fused=use_fused)
         return optimizer
+    #-------------------------------------------------------------------------
+#-------------------------------------------------------------------------
 
-# -----------------------------------------------------------------------------
+
 import tiktoken
 import numpy as np
-
+#-------------------------------------------------------------------------
 def load_tokens(filename):
     npt = np.load(filename)
     ptt = torch.tensor(npt, dtype=torch.long)
     return ptt
-
+#-------------------------------------------------------------------------
+#-------------------------------------------------------------------------
 class DataLoaderLite:
+    #-------------------------------------------------------------------------
     def __init__(self, B, T, process_rank, num_processes, split):
         self.B = B
         self.T = T
@@ -232,13 +249,15 @@ class DataLoaderLite:
         if master_process:
             print(f"found {len(shards)} shards for split {split}")
         self.reset()
-
+    #-------------------------------------------------------------------------
+    #-------------------------------------------------------------------------
     def reset(self):
         # state, init at shard zero
         self.current_shard = 0
         self.tokens = load_tokens(self.shards[self.current_shard])
         self.current_position = self.B * self.T * self.process_rank
-
+    #-------------------------------------------------------------------------
+    #-------------------------------------------------------------------------
     def next_batch(self):
         B, T = self.B, self.T
         buf = self.tokens[self.current_position : self.current_position+B*T+1]
@@ -253,7 +272,9 @@ class DataLoaderLite:
             self.current_position = B * T * self.process_rank
         return x, y
 
-# -----------------------------------------------------------------------------
+    #-------------------------------------------------------------------------
+#-------------------------------------------------------------------------
+
 # simple launch:
 # python train_gpt2.py
 # DDP launch for e.g. 8 GPUs:
@@ -283,20 +304,32 @@ else:
     ddp_local_rank = 0
     ddp_world_size = 1
     master_process = True
-    # attempt to autodetect device
-    device = "cpu"
+#-------------------------------------------------------------------------
+def get_device():
+    device = 'cpu'
     if torch.cuda.is_available():
-        device = "cuda"
-    elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
-        device = "mps"
-    print(f"using device: {device}")
+        device = 'cuda' 
+        print('using cuda acceleration')
+    # elif torch.backends.mps.is_built():
+    #     device = 'mps'
+    #     print('using mps acceleration')
+    else:
+        device = 'cpu'
+        print('using cpu')
+
+
+    return device
+#-------------------------------------------------------------------------
+device = get_device()
+import time
 
 torch.manual_seed(1337)
 if torch.cuda.is_available():
     torch.cuda.manual_seed(1337)
 
 total_batch_size = 524288 # 2**19, ~0.5M, in number of tokens
-B = 64 # micro batch size
+# B = 64 # micro batch size
+B = 16 # micro batch size
 T = 1024 # sequence length
 assert total_batch_size % (B * T * ddp_world_size) == 0, "make sure total_batch_size is divisible by B * T * ddp_world_size"
 grad_accum_steps = total_batch_size // (B * T * ddp_world_size)
@@ -321,6 +354,7 @@ max_lr = 6e-4
 min_lr = max_lr * 0.1
 warmup_steps = 715
 max_steps = 19073
+#-------------------------------------------------------------------------
 def get_lr(it):
     # 1) linear warmup for warmup_iters steps
     if it < warmup_steps:
@@ -333,13 +367,14 @@ def get_lr(it):
     assert 0 <= decay_ratio <= 1
     coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio)) # coeff starts at 1 and goes to 0
     return min_lr + coeff * (max_lr - min_lr)
-
+#-------------------------------------------------------------------------
+#-------------------------------------------------------------------------
 # optimize!
 optimizer = raw_model.configure_optimizers(weight_decay=0.1, learning_rate=6e-4, device=device)
 
 for step in range(max_steps):
     t0 = time.time()
-
+    #-------------------------------------------------------------------------
     # once in a while evaluate our validation loss
     if step % 100 == 0:
         model.eval()
@@ -358,7 +393,7 @@ for step in range(max_steps):
             dist.all_reduce(val_loss_accum, op=dist.ReduceOp.AVG)
         if master_process:
             print(f"validation loss: {val_loss_accum.item():.4f}")
-
+    #-------------------------------------------------------------------------
     # training loop
     model.train()
     optimizer.zero_grad()
@@ -385,14 +420,15 @@ for step in range(max_steps):
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
     optimizer.step()
-    torch.cuda.synchronize() # wait for the GPU to finish work
+    if torch.cuda.is_available():
+        torch.cuda.synchronize() # wait for the GPU to finish work
     t1 = time.time()
     dt = t1 - t0 # time difference in seconds
     tokens_processed = train_loader.B * train_loader.T * grad_accum_steps * ddp_world_size
     tokens_per_sec = tokens_processed / dt
     if master_process:
         print(f"step {step:5d} | loss: {loss_accum.item():.6f} | lr {lr:.4e} | norm: {norm:.4f} | dt: {dt*1000:.2f}ms | tok/sec: {tokens_per_sec:.2f}")
-
+#-------------------------------------------------------------------------
 if ddp:
     destroy_process_group()
 
@@ -411,6 +447,8 @@ x = tokens.to(device)
 # set the seed to 42
 torch.manual_seed(42)
 torch.cuda.manual_seed(42)
+
+#-------------------------------------------------------------------------
 while x.size(1) < max_length:
     # forward the model to get the logits
     with torch.no_grad():
@@ -429,9 +467,12 @@ while x.size(1) < max_length:
         xcol = torch.gather(topk_indices, -1, ix) # (B, 1)
         # append to the sequence
         x = torch.cat((x, xcol), dim=1)
-
+#-------------------------------------------------------------------------
+#-------------------------------------------------------------------------
 # print the generated text
 for i in range(num_return_sequences):
     tokens = x[i, :max_length].tolist()
     decoded = enc.decode(tokens)
     print(">", decoded)
+#-------------------------------------------------------------------------
+
