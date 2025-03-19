936f9bd8f974666449bef15ac8dfd213024fad8a
Paulo <paulotcj@gmail.com>
2025-03-19 15:27:09 -0400

    .

--- models/10_nano_gpt/current/a3_tokenization_and_sampling.py
+++ models/10_nano_gpt/current/a3_tokenization_and_sampling.py
@@ -88,9 +88,9 @@ class MLP(nn.Module):
     #-------------------------------------------------------------------------
     def __init__(self, config):
         super().__init__()
-        self.c_fc    = nn.Linear(config.n_embd, 4 * config.n_embd)
-        self.gelu    = nn.GELU(approximate='tanh')
-        self.c_proj  = nn.Linear(4 * config.n_embd, config.n_embd)
+        self.c_fc    = nn.Linear(config.n_embd, 4 * config.n_embd) # context fully connected
+        self.gelu    = nn.GELU(approximate='tanh') # GELU activation will use the tanh approximation for its computation, which can be faster than the exact computation.
+        self.c_proj  = nn.Linear(4 * config.n_embd, config.n_embd) # context projection
     #-------------------------------------------------------------------------
     #-------------------------------------------------------------------------
     def forward(self, x):
