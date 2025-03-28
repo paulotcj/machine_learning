62364cfbd53b8af2b20651330d07d32957f7cf80
Paulo <paulotcj@gmail.com>
2025-03-28 00:22:57 -0400

    .

--- models/10_nano_gpt/current/a16_hyperparameters_adamw_gradient_clipping.py
+++ models/10_nano_gpt/current/a16_hyperparameters_adamw_gradient_clipping.py
@@ -667,26 +667,34 @@ torch.cuda.manual_seed(42)
 while x.size(1) < max_length:
     # forward the model to get the logits
     with torch.no_grad():
-        logits = model(x) # (B, T, vocab_size)
-        # take the logits at the last position
-        logits = logits[:, -1, :] # (B, vocab_size)
+        logits = model(x) # (B, T, vocab_size) - [5, 8, 50257]
+        
+        # take the logits at the last position. Consider that the original logit shape is [5, 8, 50257]
+        #  if you take only the last position from the second dim, as in logits[:, -1, :], what you are
+        #  effectively doing is [5, 50257] 
+        logits_last = logits[:, -1, :] # (B, vocab_size) - [5, 50257] 
+
         # get the probabilities
-        probs = F.softmax(logits, dim=-1)
+        probs = F.softmax(input = logits_last, dim=-1)
+
         # do top-k sampling of 50 (huggingface pipeline default)
         # topk_probs here becomes (5, 50), topk_indices is (5, 50)
-        topk_probs, topk_indices = torch.topk(probs, 50, dim=-1)
+        topk_probs, topk_indices = torch.topk(input = probs, k = 50, dim=-1)
+
         # select a token from the top-k probabilities
         # note: multinomial does not demand the input to sum to 1
-        ix = torch.multinomial(topk_probs, 1) # (B, 1)
+        ix = torch.multinomial(input = topk_probs, num_samples = 1) # (B, 1)
+
         # gather the corresponding indices
-        xcol = torch.gather(topk_indices, -1, ix) # (B, 1)
+        xcol = torch.gather(input = topk_indices, dim = -1, index = ix) # (B, 1)
+
         # append to the sequence
         x = torch.cat((x, xcol), dim=1)
 #-------------------------------------------------------------------------
 #-------------------------------------------------------------------------
 # print the generated text
-for i in range(num_return_sequences):
-    tokens = x[i, :max_length].tolist()
+for i in range(num_return_sequences): # num_return_sequences = 5
+    tokens = x[i, :max_length].tolist() # max_length - 30
     decoded = enc.decode(tokens)
     print(">", decoded)
 #-------------------------------------------------------------------------
